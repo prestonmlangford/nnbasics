094352ad7c42ef0dd756b32df4a6071fe63ac3e2
prestonmlangford <prestonmlangford@gmail.com>
2020-01-12 12:23:45 -0600

    first commit

---
+++ nnmodel.py
@@ -0,0 +1,203 @@
+# Package imports
+import numpy as np
+import matplotlib.pyplot as plt
+from testCases_v2 import *
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+from planar_utils import plot_decision_boundary, load_planar_dataset, load_extra_datasets
+
+
+def relu(Z):
+    return np.maximum(0,Z)
+    
+
+def drelu(A):
+    return np.double(A>0)
+    
+    
+def tanh(Z):
+    return np.tanh(Z)
+    
+    
+def dtanh(A):
+    return 1-np.square(A)
+    #return np.ones(A.shape)
+
+def sigmoid(Z):
+    return 1/(1+np.exp(-Z))
+    
+    
+def dsigmoid(A):
+    return A*(1-A)
+    
+    
+def cross_entropy(Y,Yh):
+    
+    # n is the number of training examples
+    n = Y.shape[1]
+    
+    # to deal with numerical issues, limit Yh in (0,1)
+    epsilon = 1e-10
+    Yh = np.maximum(Yh,epsilon)
+    Yh = np.minimum(Yh,1-epsilon)
+    
+    cost = -(1/n)*(np.dot(Y,np.log(Yh.T))+np.dot(1-Y,np.log(1-Yh.T)))
+    cost = np.squeeze(cost)
+    #dYh = ((1-Yh)*Y-Yh*(1-Y))/(Yh*(1-Yh))#-(Y/Yh-(1-Y)/(1-Yh))
+    #dYh = (Yh-Y)/(Yh*(1-Yh))
+    dYh = -(Y/Yh-(1-Y)/(1-Yh))
+    
+    # returns cost and derivative of cost WRT Yh
+    return cost, dYh
+
+class FullyConnected:
+    def __init__(self,inputs,outputs,activation):
+        self.activation = activation
+        
+        # "He Initialization"
+        W_scale = np.sqrt(1/inputs)
+        self.W = np.random.randn(outputs,inputs)*W_scale
+        self.B = np.zeros((outputs,1))
+        
+        # these will get set later.  
+        # Setting them here to satisfy python class constructor
+        # d is the gradient term,
+        # v is the momentum term, 
+        # s is the second moment for RMS prop
+        self.dW = np.zeros(self.W.shape)
+        self.vW = np.zeros(self.W.shape)
+        self.sW = np.zeros(self.W.shape)
+        
+        self.dB = np.zeros(self.B.shape)
+        self.vB = np.zeros(self.B.shape)
+        self.sB = np.zeros(self.B.shape)
+        
+        # n is the number of training examples for the current iteration
+        # values of X and Y will be over written during the forward pass
+        # Y is the output of the layer
+        # X is the input to the layer
+        # these values get cached for the backwards pass
+        self.n = 1
+        self.Y  = np.zeros((outputs,1))
+        self.X  = np.zeros((inputs,1))
+    
+    def forward(self,X,update_cache = True):
+        
+        # n is the number of training examples
+        # Z[outputs,n] = W[outputs,inputs] x X[inputs,n] + B[outputs,1]
+        # B will broadcast over the second dimension
+        Z = np.dot(self.W,X)+self.B
+        
+        # choose activation set by initialization
+        if self.activation == "tanh":
+            Y = tanh(Z)
+        elif self.activation == "relu":
+            Y = relu(Z)
+        elif self.activation == "sigmoid":
+            Y = sigmoid(Z)
+        else:
+            raise ValueError("FullyConnected layer activation type not set")
+        
+        if update_cache:
+            # cache these for the backward pass to calculate gradients
+            self.X = X
+            self.Y = Y
+        
+        return Y
+    
+    
+    def backward(self,dY):
+        
+        # n is the number of training examples
+        n = dY.shape[1]
+        
+        # activation derivative must match forward pass
+        if self.activation == "tanh":
+            dG = dtanh(self.Y)
+        elif self.activation == "relu":
+            dG = drelu(self.Y)
+        elif self.activation == "sigmoid":
+            dG = dsigmoid(self.Y)
+        else:
+            raise ValueError("FullyConnected layer activation type not set")
+        
+        # dZ[outputs,n] = dY[outputs,n] * dG[outputs,n]
+        dZ = dY*dG
+        
+        # dW[outputs,inputs] = dZ[outputs,n] x dX'[n,inputs]
+        dW = (1/n)*np.dot(dZ,self.X.T)
+        
+        # dB[outputs,1] = average over n dZ[outputs,n]
+        dB = (1/n)*np.sum(dZ,axis=1,keepdims=True)
+        
+        # dX[inputs,n] = W'[inputs,outputs] x dZ[outputs,n]
+        dX = np.dot(self.W.T,dZ)
+        
+        # save these for update
+        self.dW = dW
+        self.dB = dB
+        self.n  = n
+        
+        return dX
+        
+    
+    def update(self,alpha,lambd,beta_v,beta_s,iterations):
+        
+        self.vW = beta_v*self.vW+(1-beta_v)*self.dW
+        self.vB = beta_v*self.vB+(1-beta_v)*self.dB
+        
+        # the step below is different than Adam.  I use STD instead of RMS
+        self.sW = beta_s*self.sW+(1-beta_s)*np.square(self.dW-self.vW)
+        self.sB = beta_s*self.sB+(1-beta_s)*np.square(self.dB-self.vB)
+        
+        t = iterations+1
+        vWc = self.vW/(1-np.power(beta_v,t))
+        vBc = self.vB/(1-np.power(beta_v,t))
+        sWc = self.sW/(1-np.power(beta_s,t))
+        sBc = self.sB/(1-np.power(beta_s,t))
+        
+        Wstep = alpha*vWc/(1e-8+np.sqrt(sWc))
+        Bstep = alpha*vBc/(1e-8+np.sqrt(sBc))
+        
+        # uses "weight decay".  This is equivalent to L2 regularization
+        # cost does not include the regularization cost, because why bother?
+        decay = 1-alpha*lambd/self.n
+        self.W = self.W*decay - Wstep
+        self.B = self.B*decay - Bstep
+
+# PMLFIXME implement dropout
+class NNModel:
+    def __init__(self,layers):
+        self.layers = layers
+
+
+    def forward(self,X,update_cache = True):
+        for layer in self.layers: 
+            X = layer.forward(X,update_cache)
+        return X
+
+    def backward(self,dY):
+        for layer in reversed(self.layers): 
+            dY = layer.backward(dY)
+    
+    def update(self,alpha,lambd,beta_v,beta_s,iterations):
+        for layer in self.layers: 
+            layer.update(alpha,lambd,beta_v,beta_s,iterations)
+    
+    def train(self,data,iterations,alpha,lambd,beta_v,beta_s):
+        for i in range(0,iterations):
+            X,Y = data
+            
+            Yh = self.forward(X)
+            
+            cost, dYh = cross_entropy(Y,Yh)
+            
+            self.backward(dYh)
+            
+            self.update(alpha,lambd,beta_v,beta_s,i)
+            
+            if (i%100) == 0:
+                print("Cost after " + str(i) + " iterations: " + str(cost))
+    
+
---
+++ test.py
@@ -0,0 +1,100 @@
+import nnmodel as nn
+import numpy as np
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sklearn
+import sklearn.datasets
+import sklearn.linear_model
+
+
+
+
+def plot_decision_boundary(model, data):
+    X, y = data
+    
+    # Set min and max values and give it some padding
+    x_min, x_max = X[0, :].min() - 1, X[0, :].max() + 1
+    y_min, y_max = X[1, :].min() - 1, X[1, :].max() + 1
+    h = 0.01
+    # Generate a grid of points with distance h between them
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+    # Predict the function value for the whole grid
+    Z = model.forward((np.c_[xx.ravel(), yy.ravel()]).T)
+    Z = Z.reshape(xx.shape)
+    # Plot the contour and training examples
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+    plt.ylabel('x2')
+    plt.xlabel('x1')
+    plt.scatter(X[0, :], X[1, :], c=np.ravel(y), cmap=plt.cm.Spectral)
+    
+def visualize_dataset(data):
+    X,Y = data
+    plt.scatter(X[0, :], X[1, :], c=np.ravel(Y), s=40, cmap=plt.cm.Spectral)
+    plt.draw()
+
+def make_2d_dataset(size,kind):
+    N = size
+    
+    if kind is "blobs":
+        X,Y = sklearn.datasets.make_blobs(
+            n_samples=N, 
+            #random_state=9, 
+            n_features=2, 
+            centers=5,
+            cluster_std=1)
+        Y = Y%2
+    elif kind is "circles":        
+        X,Y = sklearn.datasets.make_circles(
+            n_samples=N, 
+            factor=.5, 
+            noise=.1)
+    elif kind is "moons":
+        X,Y = sklearn.datasets.make_moons(
+            n_samples=N, 
+            noise=.2)
+    elif kind is "gaussian":    
+        X,Y = sklearn.datasets.make_gaussian_quantiles(
+            mean=None, 
+            cov=0.5, 
+            n_samples=N, 
+            n_features=2, 
+            n_classes=2, 
+            shuffle=True, 
+            random_state=None)
+            
+    # sklearn has a different convention for dataset shapes 
+    X = X.T
+    Y = Y.reshape(1, Y.shape[0])
+    
+    #normalize data
+    X = X-np.mean(X,axis=1,keepdims=True)
+    X = X/np.std(X,axis=1,keepdims=True)
+    
+    return X,Y 
+
+
+data = make_2d_dataset(400,"gaussian")
+visualize_dataset(data)
+
+mod = nn.NNModel(
+    [
+        nn.FullyConnected(2,4,"tanh"),
+        nn.FullyConnected(4,4,"tanh"),
+        nn.FullyConnected(4,1,"sigmoid"),
+    ]
+)
+
+mod.train(
+    data = data,
+    iterations = 1000,
+    alpha = 0.01,
+    lambd = 0.9,
+    beta_v = 0.9,
+    beta_s = 0.9,
+)
+
+plot_decision_boundary(mod,data)
+
+# keeps windows open after program finishes
+plt.show()
